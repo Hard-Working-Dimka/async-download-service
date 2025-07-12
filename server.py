06f8f30bb02a396365e9e35bf4223c09018abb73
Dmitry <dimarinks@yandes.ru>
2025-07-12 17:33:58 +0300

    add sendind archieves

--- server.py
+++ server.py
@@ -1,9 +1,35 @@
 from aiohttp import web
 import aiofiles
+import asyncio
+
+INTERVAL_SECS = 1
 
 
-async def archive(request):
-    raise NotImplementedError
+async def send_archieve(request):
+
+    archieve_name = request.match_info['archive_hash']
+    path = f'test_photos/{archieve_name}'
+
+    response = web.StreamResponse()
+    response.headers['Content-Disposition'] = f'attachment; filename="Photos.zip"'
+
+    await response.prepare(request)
+
+    process = await asyncio.create_subprocess_exec(
+        'zip', '-r', '-', '.',
+        stdout=asyncio.subprocess.PIPE,
+        cwd=path
+    )
+
+    while True:
+        chunk = await process.stdout.read(512000)
+        if not chunk:
+            await response.write_eof()
+            break
+        await response.write(chunk)
+
+    await process.wait()
+    return response
 
 
 async def handle_index_page(request):
@@ -16,6 +42,6 @@ if __name__ == '__main__':
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
-        web.get('/archive/{archive_hash}/', archive),
+        web.get('/archive/{archive_hash}/', send_archieve),
     ])
     web.run_app(app)
