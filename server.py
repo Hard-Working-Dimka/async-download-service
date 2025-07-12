f0f5cf05040c36c6a310b110258415a703af79eb
Dmitry <dimarinks@yandes.ru>
2025-07-12 22:04:48 +0300

    add env and arg for commandline

--- server.py
+++ server.py
@@ -2,14 +2,19 @@ from aiohttp import web
 import aiofiles
 import asyncio
 import os
+import logging
+from environs import Env
+import argparse
+import configargparse
 
-INTERVAL_SECS = 1
+
+SIZE_OF_CHUNK = 409600
 
 
 async def send_archieve(request):
 
-    archieve_name = request.match_info['archive_hash']
-    path = f'test_photos/{archieve_name}'
+    archive_hash = request.match_info['archive_hash']
+    path = f'{args.path}/{archive_hash}'
 
     if not os.path.exists(path):
         raise web.HTTPNotFound(
@@ -26,15 +31,20 @@ async def send_archieve(request):
         cwd=path
     )
 
-    while True:
-        chunk = await process.stdout.read(512000)
-        if not chunk:
-            await response.write_eof()
-            break
-        await response.write(chunk)
-
-    await process.wait()
-    return response
+    try:
+        while True:
+            chunk = await process.stdout.read(SIZE_OF_CHUNK)
+            if not chunk:
+                break
+            await response.write(chunk)
+            logging.info('Sending archive chunk')
+            await asyncio.sleep(args.delay)
+    except asyncio.CancelledError:
+        logging.warning('Download was interrupted')
+    finally:
+        await process.wait()
+        await response.write_eof()
+        return response
 
 
 async def handle_index_page(request):
@@ -44,6 +54,25 @@ async def handle_index_page(request):
 
 
 if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+
+    command_line_parser = configargparse.ArgumentParser(
+        description='Запуск микросервиса для отправки архивов'
+    )
+    command_line_parser.add_argument('-p', '--path', type=str, default='test_photos', env_var='PATH_TO_PHOTOS',
+                                     help='Путь до каталога, в которой лежат папки с фотографиями')
+    command_line_parser.add_argument('-d', '--delay', type=int, default=1,
+                                     env_var='PAUSE_TIME_OF_DOWNLOADING', help='Замедление загрузки архива')
+    command_line_parser.add_argument(
+        '-l', '--logging', action='store_true', help='Включить логирование')
+
+    args = command_line_parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG)
+    if not args.logging:
+        logging.disable(logging.CRITICAL)
+
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
